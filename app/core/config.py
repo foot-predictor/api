9a748f95765c0aeab5e1f146426d76f6ac16e12a
Alexis Thibault <alexis.thibault31@gmail.com>
2025-01-01 19:39:55 +0100

    :rocket: add connection to database

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,10 @@
 import secrets
 from functools import lru_cache
+from pathlib import Path
 from typing import Literal
 
+from pydantic import computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,11 +16,31 @@ class Settings(BaseSettings):
     DESCRIPTION: str
     VERSION: str
 
-    API_STR: str = "/api/v"
     SECRET_KEY: str = secrets.token_urlsafe(32)
+    BASE_DIR: Path = Path(__file__).parent.parent
+
+    API_STR: str = "/api/v"
 
     ENVIRONMENT: Literal["development", "staging", "production"] = "development"
     DEBUG: bool = ENVIRONMENT != "production"
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @computed_field  # type: ignore
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> MultiHostUrl:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
 
 
 @lru_cache
